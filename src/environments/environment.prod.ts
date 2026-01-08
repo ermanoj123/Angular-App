04120a01c289848863cebdadfb7573d95f0e89fc
ermanoj123 <er.manoj2016@gmail.com>
2026-01-08 09:05:32 +0545

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://192.168.200.141:8000/api'
+  production: false,
+  apiUrl: 'http://192.168.1.68:8000/api' // Updated to match your current Wi-Fi IP
 };
