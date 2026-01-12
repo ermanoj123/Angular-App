b94490e897ed702c2d14b4c27dba0c40311efd94
ermanoj123 <er.manoj2016@gmail.com>
2026-01-12 11:12:32 +0545

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.200.102:8000/api' // Matches your current "SYNERGY" Wi-Fi IP
+  apiUrl: 'http://192.168.200.119:8000/api' // Matches your current "SYNERGY" Wi-Fi IP
 };
